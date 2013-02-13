75b45b34bb80110acebc8cad812530165cd0ab02
mistral7 <mistral7@gmail.com>
2013-02-13 16:36:01 +0900

    Add asserts test case.

--- test/test_version.rb
+++ test/test_version.rb
@@ -6,4 +6,28 @@ class TestVersion < Test::Unit::TestCase
   def test_version
     assert_equal("0.0.1", Hostsan::VERSION)
   end
+
+  def test_assert_equal
+    # ここで assert_equal を試す
+    assert_equal('hoge', 'hoge')
+  end
+
+  def test_assert_raise
+    # ここで assert_raise を試す
+    assert_raise(RuntimeError) {
+      raise 'Exception'
+    }
+  end
+
+  def test_assert_nothing_raised
+    # ここで assert_nothing_raised を試す
+    assert_nothing_raised(true) {
+      true
+    }
+  end
+
+  def test_assert
+    # ここで assert を試す
+    assert(true)
+  end
 end
